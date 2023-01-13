1444b5402adfa8714986d1f89c4fe2c8f7c85975
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-01-13 11:17:31 -0800

    changed email for sender

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -6,7 +6,7 @@ async function sendEmail(req, res) {
   try {
     await sendgrid.send({
       to: "alexsandrr2005@gmail.com", // Your email where you'll receive emails
-      from: "aleksandr.riabov@csedge.org", // your website email address here
+      from: "alexriabov00@gmail.com", // your website email address here
       subject: `[Lead from website] : ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">

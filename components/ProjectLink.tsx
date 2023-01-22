8d49a6d7bc9c8089cc2a31b2cf641900a1d94b49
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-01-22 10:32:15 -0800

    Update ProjectLink.tsx

--- components/ProjectLink.tsx
+++ components/ProjectLink.tsx
@@ -10,15 +10,15 @@ export default function ProjectLink({slug, file, title, description}) {
     href={`/projects/${slug}`}
     className={cn(
       'transform hover:scale-[1.01] transition-all',
-      'rounded-xl w-full bg-black p-1'
+      'rounded-xl w-full bg-black p-1 mx-auto'
     )}
   >
     <div className="flex flex-col justify-between h-full shadow-2xl bg-white dark:bg-[#18222d] rounded-lg p-4">
       <div className="flex flex-col md:flex-col justify-between">
 
-      <div className="relative ">
+      <div className="relative flex ">
         <a className="absolute inset-0 z-10 bg-black text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-          <h1  className="mx-auto text-white" >{description}</h1>
+          <h1  className="mx-auto text-[3vw] flex text-white" >{description}</h1>
         </a>
         <div className="flex flex-wrap content-center">
           <img src={file} alt={file} className="mx-auto filter w-auto h-auto"/>

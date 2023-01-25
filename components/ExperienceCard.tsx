0ac43c8ed1433e4a9ca59a1a9b76d51ab27ba8ac
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-01-24 20:33:39 -0800

    Update ExperienceCard.tsx

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -6,8 +6,8 @@ export default function ExperienceCard({title, name, date, description,letter}) 
                 <div className="timeline-dot"><h6>{letter}</h6></div>
                 <div className="card timeline-content">
                     <div className="card-content bg-slate-100 dark:bg-slate-600 ">
-                        <h6 className="timeline-title dark:text-white ">{title}</h6>
-                        <div className="timeline-info dark:text-white ">
+                        <h6 className="timeline-title text-black dark:text-white ">{title}</h6>
+                        <div className="timeline-info text-black dark:text-white ">
                             <h6>
                                 <small>{name}</small>
                             </h6>
@@ -15,7 +15,7 @@ export default function ExperienceCard({title, name, date, description,letter}) 
                                 <small>{date}</small>
                             </h6>
                         </div>
-                        <p className='dark:text-white m-auto w-auto'>
+                        <p className='text-black dark:text-white m-auto w-auto'>
                             {description}
                         </p>
                     </div>

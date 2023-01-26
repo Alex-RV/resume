ddd17487d4765008b180579ba94ea2bad62e19cd
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-01-25 19:10:03 -0800

    fixed contact api for vercel

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -1,8 +1,10 @@
-export default function (req, res) {
-    require('dotenv').config()
-    const PASSWORD = process.env.password
+const nodemailer = require("nodemailer");
 
-    let nodemailer = require('nodemailer')
+export default async (req, res) => {
+
+  // const { fullname, subject, email, message } = JSON.parse(req.body);
+    // require('dotenv').config()
+    // let nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
       host: "smtp-mail.outlook.com", // hostname
       secureConnection: false, // use SSL
@@ -14,7 +16,19 @@ export default function (req, res) {
       tls: {
         ciphers:'SSLv3'
     }
-    })
+    });
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              console.log("Server is ready to take our messages");
+              resolve(success);
+          }
+      });
+  });
     const mailData = {
       from: 'alex-riabov-resume@outlook.com',
       to: 'alexsandrr2005@gmail.com',
@@ -49,12 +63,18 @@ export default function (req, res) {
       </body>
       </html>`,
     }
-    transporter.sendMail(mailData, function (err, info) {
-      if(err)
-        console.log(err)
-      else
-        console.log(info)
-    })
-    res.status(200)
-    res.send()
-  }
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailData, (err, info) => {
+          if (err) {
+              console.error(err);
+              reject(err);
+          } else {
+              console.log(info);
+              resolve(info);
+          }
+      });
+  });
+  
+  res.status(200).json({ status: "OK" });
+  };

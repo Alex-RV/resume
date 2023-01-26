80b8055f6affcb0061b71b21d5c4f60dcd15c9d8
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-01-26 09:21:43 -0800

    possible fix env for the vercel

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -1,6 +1,6 @@
 export default async (req, res) => {
   require('dotenv').config()
-  const PASSWORD = process.env.password
+  const PASSWORD_KEY = process.env.PASSWORD_KEY
 
   let nodemailer = require('nodemailer')
   const transporter = nodemailer.createTransport({
@@ -9,7 +9,7 @@ export default async (req, res) => {
     port: 587, // port for secure SMTP
     auth: {
       user: 'alex-riabov-resume@outlook.com',
-      pass: 'PasswordResume',
+      pass: PASSWORD_KEY,
     },
     tls: {
       ciphers:'SSLv3'

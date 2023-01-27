b5a942a761791055844fd2770271358f1bd25422
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-01-26 21:19:40 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from 'next-themes'
 import { Inter } from '@next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { useState, useEffect } from 'react';
+import Aos from 'aos';
+import "aos/dist/aos.css"
 
 
 
@@ -11,6 +13,9 @@ const interVariable = Inter();
 var age: number;
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Aos.init({duration: 2000});
+  }, []);
   useEffect(() => {
     console.log(globalThis.age)
     if(globalThis.age == ""|| null || undefined || "undefined"){

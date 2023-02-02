27c8eb9fe2c3db55d875dd5e66a3c3b022c6c129
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-02-02 08:31:22 -0800

    Update SkillsCard.tsx

--- components/SkillsCard.tsx
+++ components/SkillsCard.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 
 export default function SkillsCard({scale, color, description, img, level}) {
   return (
-    <div data-aos="zoom-in">
+    <div data-aos="zoom-in" 
+     data-aos-offset="0">
     <div className='transform hover:scale-[1.01] transition-all'>
       <div className='flex flex-row rounded-2xl mb-10 mx-auto bg-gray-200 dark:bg-slate-600 max-w-2xl w-full transform hover:scale-[1.01] transition-all'>
         <div className='flex p-4 items-center bg-cover bg-center bg-no-repeat'>

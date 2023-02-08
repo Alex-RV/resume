d7176d49e33c36c8c6377f1376be4d1fbdaa400f
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-02-07 16:05:25 -0800

    Update ip.tsx

--- pages/ip.tsx
+++ pages/ip.tsx
@@ -1,29 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from '../components/Container';
+
+
 
 export default function Ip() {
     const [dataIP, setDataIP] = useState('')
     const [country_code, setCountry_code] = useState('')
     const [country_name, setCountry_name] = useState('')
     const [city, setCity] = useState('')
-    // const [dataIP, setDataIP] = useState('')
-    // const [dataIP, setDataIP] = useState('')
-    // const [dataIP, setDataIP] = useState('')
-    
-    function getIP() {
-        let data  = ''
-        fetch('https://api.ipify.org?format=json')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(myJson) {
-
-            data=myJson.ip
-        setDataIP(data)
-        });
-      }
-      function getIPData() {
-        let data,country_code,country_name,city,postal,latitude,longitude,IPv4,state  = ''
+    const [postal, setPostal] = useState('')
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+    const [IPv4, setIPv4] = useState('')
+    const [state, setState] = useState('')
+    useEffect(() => {
+      let data,country_code,country_name,city,postal,latitude,longitude,IPv4,state  = ''
         
         fetch('https://geolocation-db.com/json/')
         .then(function(response) {
@@ -42,20 +33,38 @@ export default function Ip() {
         setDataIP(data)
         setCountry_code(country_code)
         setCountry_name(country_name)
-        setCountry_name(city)
-        setCity
+        setCity(city)
+        setPostal(postal)
+        setLatitude(latitude)
+        setLongitude(longitude)
+        setIPv4(IPv4)
+        setState(state)
         });
-      }
+            fetch('https://api.ipify.org?format=json')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(myJson) {
+
+                data=myJson.ip
+            setDataIP(data)
+            });
+    }, [])
   return (
     <Container>
         <div className="flex flex-col items-start max-w-2xl w-full mx-auto mb-16">
-        <ul className="font-bold justify-start text-3xl md:text-5xl tracking-tight mb-16 text-black dark:text-white">
+        <ul className="font-bold grid-cols-2 justify-start text-3xl md:text-5xl tracking-tight mb-16 text-black dark:text-white">
           <li>IP : {dataIP}</li>
           <li>Country Code : {country_code}</li>
           <li>Country Name : {country_name}</li>
           <li>City : {city}</li>
+          <li>Postal : {postal}</li>
+          <li>Latitude : {latitude}</li>
+          <li>Longitude : {longitude}</li>
+          <li>IPv4 : {IPv4}</li>
+          <li>State : {state}</li>
         </ul>
-        <button role="button"className='button-ip' onClick={() =>getIPData()}>Get my IP</button>
+        {/* <button role="button"className='button-ip' onClick={() =>getIPData()}>Get my IP</button> */}
     </div>
     </Container>
     

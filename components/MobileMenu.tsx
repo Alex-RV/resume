0958e7ac640fda9c1136659ea3175ff91a61fe40
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-02-18 11:33:20 -0800

    changed mobile nav bar

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -46,7 +46,7 @@ export default function MobileMenu() {
         <ul
           className={cn(
             styles.menu,
-            'flex flex-col absolute bg-gray-100 dark:bg-gray-900',
+            'flex flex-col absolute bg-gray-100 rounded-b-xl dark:bg-gray-900',
             isMenuRendered && styles.menuRendered
           )}
         >

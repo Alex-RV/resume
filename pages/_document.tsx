33b486a3e2d3fe2195b043640119572523964b0b
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-02-18 21:09:17 -0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <meta name="google-site-verification" content="AByyjgspGOVnEQSxTYO7SUBa2Af93DGcOaqthjhoDR8" />
       <link href="/favicons/favicon.ico" rel="shortcut icon"/>
       <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>

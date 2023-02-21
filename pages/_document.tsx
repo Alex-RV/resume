a915f59c77977074d1f23be3ba82d0b5341f9094
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-02-20 20:03:37 -0800

    fixing favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,13 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <meta name="google-site-verification" content="AByyjgspGOVnEQSxTYO7SUBa2Af93DGcOaqthjhoDR8" />
-      <link href="/favicons/favicon.ico" rel="shortcut icon"/>
-      <link rel="icon" href="/favicons/favicon.ico"/>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
-      <link rel="manifest" href="/favicons/site.webmanifest"/>
-      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5"/>
+      <link href="favicon.ico" rel="shortcut icon"/>
+      <link rel="icon" href="favicon.ico"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"/>
+      <link rel="manifest" href="site.webmanifest"/>
+      <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5"/>
       <meta name="msapplication-TileColor" content="#da532c"/>
       <meta name="theme-color" content="#ffffff"/>
       </Head>

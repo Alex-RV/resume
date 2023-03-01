5f10e0b00d7c700898c3ad3de3cc5b3a7cf6adda
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-03-01 14:51:18 -0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
       <meta name="google-site-verification" content="AByyjgspGOVnEQSxTYO7SUBa2Af93DGcOaqthjhoDR8" />
       <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
       <link href="/static/favicons/site.webmanifest" rel="manifest" />
+      <link rel="icon" href="/static/favicons/favicon.ico"/>
       <link rel="shortcut icon" sizes="196x196" href="/static/favicons/android-chrome-192x192.png"/>
       <link href="/static/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
       <link href="/static/favicons/favicon-144x144.png" rel="icon" sizes="144x144" type="image/png" />

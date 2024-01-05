e6adffda1a897bcbe7b0130da871ea2f98b87042
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2024-01-04 17:46:18 -0800

    [add]: updated types for google event response

--- lib/google/types.google.ts
+++ lib/google/types.google.ts
@@ -34,6 +34,51 @@ export interface CalendarEvent {
     };
     iCalUID: string;
     sequence: number;
+    attendees?: {
+        email: string;
+        self?: boolean;
+        responseStatus: string;
+        organizer?: boolean;
+      }[];
+    extendedProperties?: {
+      shared: {
+        meetingId: string;
+        zmMeetingNum: string;
+        meetingParams: string;
+      };
+    };
+    conferenceData?: {
+      entryPoints: {
+        entryPointType: string;
+        uri: string;
+        label?: string;
+        meetingCode?: string;
+        passcode?: string;
+        regionCode?: string;
+      }[];
+      conferenceSolution: {
+        key: {
+          type: string;
+        };
+        name: string;
+        iconUri: string;
+      };
+      conferenceId: string;
+      notes: string;
+      parameters: {
+        addOnParameters: {
+          parameters: {
+            scriptId: string;
+            realMeetingId: string;
+            creatorUserId: string;
+            meetingUuid: string;
+            meetingType: string;
+            originalEventId: string;
+          };
+        };
+      };
+    };
+    guestsCanModify?: boolean;
     reminders: {
       useDefault: boolean;
     };

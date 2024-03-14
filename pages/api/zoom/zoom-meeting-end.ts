2f09592debbe90448155689d8af921c8af37f9b3
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2024-03-14 11:28:05 -0700

    [fixing]: endpoint email ending

--- pages/api/zoom/zoom-meeting-end.ts
+++ pages/api/zoom/zoom-meeting-end.ts
@@ -1,37 +1,44 @@
 // pages/api/zoom/zoom-meeting-end.ts
-
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const event = req.body;
 
-        // Check if the event is 'meeting.ended'
         if (event.event === 'meeting.ended') {
             console.log('Meeting Ended:', event.payload.object);
-            // Handle the end meeting event here (e.g., logging, notifications, etc.)
+
             let data = {
                 fullname: "Zoom",
                 email: "zoom",
                 subject: "meeting end",
-                message: event.payload.object,
-              };
-              fetch('https://ariabov.tech/api/contact', {
-                method: 'POST',
-                headers: {
-                  'Accept': 'application/json, text/plain, */*',
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-              })
+                message: JSON.stringify(event.payload.object),
+            };
 
-            // Respond to Zoom to acknowledge receipt of the event
+            try {
+                const contactResponse = await fetch('https://ariabov.tech/api/contact', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                if (contactResponse.status === 200) {
+                    console.log('Response succeeded!');
+                } else {
+                    console.log('Response not succeeded, status code:', contactResponse.status);
+                }
+            } catch (error) {
+                console.error('Error sending request to /api/contact:', error);
+            }
+
             res.status(200).json({ message: 'Event received' });
         } else {
             res.status(200).json({ message: 'Event not related to meeting end' });
         }
     } else {
-        // Handle any non-POST requests
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }

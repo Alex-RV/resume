5acc8656437806cbb1c331ce2798e3109ae0cf6f
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2024-03-14 10:36:43 -0700

    [add]: build fix

--- sanityresume/schemas/blockContent.ts
+++ sanityresume/schemas/blockContent.ts
@@ -22,47 +22,47 @@ export default defineType({
       // correspond with HTML tags, but you can set any title or value
       // you want and decide how you want to deal with it where you want to
       // use your content.
-      styles: [
-        {title: 'Normal', value: 'normal'},
-        {title: 'H1', value: 'h1'},
-        {title: 'H2', value: 'h2'},
-        {title: 'H3', value: 'h3'},
-        {title: 'H4', value: 'h4'},
-        {title: 'li', value: 'li'},
-        {title: 'Quote', value: 'blockquote'},
-      ],
-      lists: [
-        {title: 'Bullet', value: 'bullet'},
-        { title: 'Numbered', value: 'number' },],
+      // styles: [
+      //   {title: 'Normal', value: 'normal'},
+      //   {title: 'H1', value: 'h1'},
+      //   {title: 'H2', value: 'h2'},
+      //   {title: 'H3', value: 'h3'},
+      //   {title: 'H4', value: 'h4'},
+      //   {title: 'li', value: 'li'},
+      //   {title: 'Quote', value: 'blockquote'},
+      // ],
+      // lists: [
+      //   {title: 'Bullet', value: 'bullet'},
+      //   { title: 'Numbered', value: 'number' },],
       // Marks let you mark up inline text in the block editor.
-      marks: {
-        // Decorators usually describe a single property – e.g. a typographic
-        // preference or highlighting by editors.
-        decorators: [
-          {title: 'Strong', value: 'strong'},
-          {title: 'Emphasis', value: 'em'},
-          {title: 'Highlight', value: 'highlight' },
-          {title: 'RedText', value: 'red'},
-          {title: 'BlueText', value: 'blue'},
-          {title: 'GreenText', value: 'green'},
-          {title: 'Bullet', value: 'bullet'},
-        ],
-        // Annotations can be any object structure – e.g. a link or a footnote.
-        annotations: [
-          {
-            title: 'URL',
-            name: 'link',
-            type: 'object',
-            fields: [
-              {
-                title: 'URL',
-                name: 'href',
-                type: 'url',
-              },
-            ],
-          },
-        ],
-      },
+      // marks: {
+      //   // Decorators usually describe a single property – e.g. a typographic
+      //   // preference or highlighting by editors.
+      //   decorators: [
+      //     {title: 'Strong', value: 'strong'},
+      //     {title: 'Emphasis', value: 'em'},
+      //     {title: 'Highlight', value: 'highlight' },
+      //     {title: 'RedText', value: 'red'},
+      //     {title: 'BlueText', value: 'blue'},
+      //     {title: 'GreenText', value: 'green'},
+      //     {title: 'Bullet', value: 'bullet'},
+      //   ],
+      //   // Annotations can be any object structure – e.g. a link or a footnote.
+      //   annotations: [
+      //     {
+      //       title: 'URL',
+      //       name: 'link',
+      //       type: 'object',
+      //       fields: [
+      //         {
+      //           title: 'URL',
+      //           name: 'href',
+      //           type: 'url',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
     }),
     // You can add additional types here. Note that you can't use
     // primitive types such as 'string' and 'number' in the same array

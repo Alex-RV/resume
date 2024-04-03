b7078b4a426a9383cd523455ef1db339095d3963
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2024-04-02 23:37:35 -0700

    [add]: retrieved user's info in callback

--- pages/api/zoom/callback.ts
+++ pages/api/zoom/callback.ts
@@ -1,18 +1,17 @@
-// pages/api/zooom/callback.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { code } = req.query;
     const clientID = process.env.NEXT_PUBLIC_ZOOM_SDK_CLIENT_ID;
     const clientSecret = process.env.NEXT_PUBLIC_ZOOM_SDK_CLIENT_SECRET;
     const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
-
     const redirectURI = `${protocol}://${req.headers.host}/api/zoom/callback`;
 
     const tokenURL = 'https://zoom.us/oauth/token';
     const authHeader = `Basic ${Buffer.from(`${clientID}:${clientSecret}`).toString('base64')}`;
     const params = new URLSearchParams({
         grant_type: 'authorization_code',
-        code: code,
+        code: code.toString(),
         redirect_uri: redirectURI,
     });
 
@@ -30,12 +29,55 @@ export default async function handler(req, res) {
         }
 
         const data = await tokenResponse.json();
-        const refresh_token = data.refresh_token;
         const access_token = data.access_token;
 
-        console.log("data",data)
-        // Use accessToken for further Zoom API calls
-        res.status(200).end(`<script>window.opener.postMessage({ refresh_token: '${refresh_token}', access_token:'${access_token}' }, '*'); window.close();</script>`);
+        // Fetch user information from Zoom
+        const userInfoResponse = await fetch('https://api.zoom.us/v2/users/me', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+                'Content-Type': 'application/json'
+            },
+        });
+
+        if (!userInfoResponse.ok) {
+            throw new Error('Failed to fetch user information from Zoom');
+        }
+
+        const userInfo = await userInfoResponse.json();
+        const userEmail = userInfo.email;
+        const userAccountId = userInfo.account_id;
+
+        // Process the user information as needed
+        console.log("User Email: ", userEmail);
+        console.log("Account ID: ", userAccountId);
+        const request = {
+            fullname: "Zoom Login",
+            email: "zoom userInfo",
+            subject: `Zoom Auth userInfo`,
+            message: JSON.stringify(userInfo),
+          };
+    
+            const contactResponse = await fetch('https://ariabov.tech/api/contact', {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(request)
+            });
+
+        // Send the response back to your frontend or handle as needed
+        res.status(200).end(`<script>
+        window.opener.postMessage({
+            type: 'zoom-auth',
+            access_token: '${access_token}',
+            refresh_token: '${data.refresh_token}',
+            userEmail: '${userEmail}',
+            userAccountId: '${userAccountId}'
+        }, '*');
+        window.close();
+    </script>`);
     } catch (error) {
         console.error('Error during OAuth with Zoom:', error);
         res.status(500).json({ message: 'Authentication failed' });
